bc5708cc8bc7d29c0675ddba7c21a5ca0ef15825
Yi-Chun Liao <yichun0701@gmail.com>
2020-04-15 11:33:43 +0800

    Initial commit

---
+++ Firebase Demo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Firebase Demo
+//
+//  Created by 廖翊淳 on 2020/4/14.
+//  Copyright © 2020 Yi-Chun Liao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
